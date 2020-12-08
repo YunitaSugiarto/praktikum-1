aeea0ae895018c29b630c72988b3b188306f0660
YunitaSugiarto <41984199+YunitaSugiarto@users.noreply.github.com>
2020-12-08 23:03:17 +0700

    Add files via upload

---
+++ variabel.js
@@ -0,0 +1,35 @@
+let nama = "Yunita" //menyimpan data string
+let umur = 17 //menyimpan data numerik
+let status_menikah = false //menyimpan data boolean
+
+const url = "http://smktelkom-mlg.sch.id"
+const port = 8080
+
+let nama_siswa1 = "Kiming"
+let nis_siswa1 = "101"
+let jurusan_siswa1 = "RPL"
+
+let nama_siswa2 = "Bonon"
+let nis_siswa2 = "102"
+let jurusan_siswa2 = "TKJ"
+
+let siswa1 = {
+    nis: "101",
+    nama: "Kiming",
+    jurusan: "RPL"
+}
+
+let siswa2 = {
+    nis: "102",
+    nama: "Bonon",
+    jurusan: "TKJ"
+}
+
+//menampilkan nama siswa1
+console.log("Nama Siswa 1 = " + siswa1.nama);
+
+//mengubah nama siswa 1 menjadi Noa Kazama
+siswa1.nama = "Kiming Bonon"
+
+//menampilkan nama setelah diubah
+console.log("Nama Siswa 1 = " + siswa1.nama);
